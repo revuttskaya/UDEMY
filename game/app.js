866ed2bb1bd2611058304b7a9ffa80a8f7053914
krevutskaya <revutskaya.ke@gmail.com>
2020-03-29 21:42:00 +0300

    Update score and change the active player

--- game/app.js
+++ game/app.js
@@ -4,8 +4,6 @@ scores = [0, 0];
 roundScore = 0;
 activePlayer = 0;
 
-//document.querySelector('#current-' + activePlayer).textContent = dice;
-
 document.getElementById('score-0').textContent = '0';
 document.getElementById('score-1').textContent = '0';
 document.getElementById('current-0').textContent = '0';
@@ -23,8 +21,24 @@ document.querySelector('.btn-roll').addEventListener('click', function () {
     //3. Update round score If the rolled number was not 1
     if (dice !== 1) {
         //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active');
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        /*document.querySelector('.player-0-panel').classList.remove('active');
+        document.querySelector('.player-1-panel').classList.add('active');*/
+
+        document.querySelector('.dice').style.display = 'none';
+
     }
 
 });
