77e1ac6483044d3e8a6b57b03d0418fc558814c1
krevutskaya <revutskaya.ke@gmail.com>
2020-04-14 23:51:43 +0300

    Learn Closures (the second example)

--- lesson63/script.js
+++ lesson63/script.js
@@ -16,3 +16,22 @@ retirementUS(1990);
 retirementDE(1963);
 
 // Interview questions
+
+function interviewQuestions(job) {
+    var question;
+
+    return function (name) {
+        if (job === 'driver') {
+            question = ', what car do you drive?';
+        } else if (job === 'teacher') {
+            question = ', what do you teach?';
+        } else {
+            question = ', what do you do?';
+        }
+        console.log(name + question)
+    }
+}
+
+interviewQuestions('teacher')('Mike');
+interviewQuestions('driver')('John');
+interviewQuestions('student')('Anna');
